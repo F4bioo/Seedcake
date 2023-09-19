87133c1854b47f884c55cf46739c0cb042f56a1e
F4bioo <costa.fbo@gmail.com>
2023-09-19 17:21:07 -0300

    create buildSrc

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,10 @@
+object Config {
+    private const val packageName: String = "com.fappslab.seedcake"
+    const val namespace: String = packageName
+    const val compileSdk: Int = 33
+    const val applicationId: String = packageName
+    const val minSdk: Int = 28
+    const val targetSdk: Int = 33
+    const val versionCode: Int = 1
+    const val versionName: String = "1.0"
+}
