50c27d22b2f60cd4575175ff68340a07902b817e
GitHub Action <action@github.com>
2023-09-24 02:28:37 +0000

    Changed: Bump app version v1.2023.09.24 build 309240228

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -5,6 +5,6 @@ object Config {
     const val applicationId: String = packageName
     const val minSdk: Int = 28
     const val targetSdk: Int = 33
-    const val versionCode: Int = 309240204
+    const val versionCode: Int = 309240228
     const val versionName: String = "1.2023.09.24"
 }
